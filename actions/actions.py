d30a1ae8587b2cf093be79f51c0c608fb5caf02e
Yusen95 <zhang.10095@buckeyemail.osu.edu>
2023-02-03 19:03:02 -0500

    first change

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +26,35 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+class TypeNameAction(Action):
+    def name(self) -> Text:
+        return "action_get_type_name"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, any]]:
+        
+        return [SlotSet("Name", "Whatever song it is")]
+    
+class TypeSingerAction(Action):
+    def name(self) -> Text:
+        return "action_get_type_singer"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, any]]:
+        
+        return [SlotSet("Singer", "Whoever it is")]
+    
+class TypeAlbumAction(Action):
+    def name(self) -> Text:
+        return "action_get_type_album"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, any]]:
+        
+        return [SlotSet("Album", "Whatever album it blongs to")]
